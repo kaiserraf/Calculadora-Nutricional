ff42a10562a2dc8fc2e25437e3fc8eebdcd8c52f
Rafael Machado <rafaelmachado2507oliveira@hotmail.com>
2025-10-21 13:59:01 -0300

    new branch

--- Caculadora Nutricional/Program.cs
+++ Caculadora Nutricional/Program.cs
@@ -31,19 +31,19 @@ namespace calcnutri
         // informações principais para cadastro
         public static Dictionary<int, String> idNome = new Dictionary<int, string>(); // Id - Nome
         public static Dictionary<int, float> idPeso = new Dictionary<int, float>(); // Id - Peso
-        public static Dictionary<int, String> idIdade = new Dictionary<int, string>(); // Id - Idade
-        public static Dictionary<int, float> idAltura = new Dictionary<int, float>(); // Id - Altura
+        public static Dictionary<int, int> idIdade = new Dictionary<int, int>(); // Id - Idade
+        public static Dictionary<int, int> idAltura = new Dictionary<int, int>(); // Id - Altura
         public static Dictionary<int, char> idGenero = new Dictionary<int, char>(); // Id - Genero
 
         // informações preenchidas conforme ocorre consultas
-        public static Dictionary<int, String> idKcal = new Dictionary<int, string>(); // Id - kcal
-        public static Dictionary<int, String> idPtn = new Dictionary<int, string>(); // Id - PTN
-        public static Dictionary<int, String> idLip = new Dictionary<int, string>(); // Id - LIP
-        public static Dictionary<int, String> idCho = new Dictionary<int, string>(); // Id - CHO
-        public static Dictionary<int, String> idBf = new Dictionary<int, string>(); // Id - %BF
-        public static Dictionary<int, String> idTmb = new Dictionary<int, string>(); // Id - TMB
-        public static Dictionary<int, String> idFaf = new Dictionary<int, string>(); // Id - FAF
-        public static Dictionary<int, String> idGet = new Dictionary<int, string>(); // Id - GET
+        public static Dictionary<int, float> idKcal = new Dictionary<int, float>(); // Id - kcal
+        public static Dictionary<int, float> idPtn = new Dictionary<int, float>(); // Id - PTN
+        public static Dictionary<int, float> idLip = new Dictionary<int, float>(); // Id - LIP
+        public static Dictionary<int, float> idCho = new Dictionary<int, float>(); // Id - CHO
+        public static Dictionary<int, float> idBf = new Dictionary<int, float>(); // Id - %BF
+        public static Dictionary<int, float> idTmb = new Dictionary<int, float>(); // Id - TMB
+        public static Dictionary<int, float> idFaf = new Dictionary<int, float>(); // Id - FAF
+        public static Dictionary<int, float> idGet = new Dictionary<int, float>(); // Id - GET
 
         // cadastro de novo paciente
         public static void Cadastro()
@@ -54,7 +54,7 @@ namespace calcnutri
         // menu de opções (Distribuição de Macro -  %BF - TMB)
         public static void MenuOpcoes()
         {
-
+            Console.WriteLine("Bem vindo | escolha uma opção | 1 - novo paciente | 2 - paciente antigo");
         }
         
         // metodo principal do código
